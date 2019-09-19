8bb89a0f48f035f44207657b49ae5d678b3a0fdc
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2019-09-19 22:12:46 +0900

    :sushi:Complete

--- content.js
+++ content.js
@@ -1,26 +1,17 @@
-window.onload = function() {
-    const target = document.getElementById('movie_player');
 
-    const observer = new MutationObserver(function() {
-        let ads = document.querySelectorAll(".ytp-ad-overlay-slot");
-        for(let i=0; i<ads.length; ++i) {
-            ads[i].parentNode.removeChild(ads[i]);
+window.addEventListener('keydown', function(e) {
+    const key_Enter = 13;
+
+    if(e.keyCode == key_Enter) {
+        let ad1 = document.getElementsByClassName("ytp-ad-overlay-close-container")[0];
+        let ad2 = document.getElementsByClassName("ytp-ad-skip-button-container")[0];
+
+        if (ad1) {
+            ad1.click();
         }
-        //let elem1 = document.getElementsByClassName("ytp-ad-overlay-close-container")[0];
-        //let elem2 = document.getElementsByClassName("ytp-ad-skip-button-container")[0];
-        //if (elem1) {
-        //    elem1.parentNode.removeChild(elem1);
-        //    console.log("emem1");
-        //}
-        //if (elem2) {
-        //    elem2.parentNode.removeChild(elem2);
-        //    console.log("emem2");
-        //}
-        console.log(target.children.length);
-    });
-
-    const config = { childList: true, attributes: true, characterData: true };
-
-    observer.observe(target);
-};
+        if (ad2) {
+            ad2.click();
+        }
+    }
+});
 
