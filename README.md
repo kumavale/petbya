ae38271d757e9e3b89e3b7f5739847add8998685
kumavale <29778890+kumavale@users.noreply.github.com>
2022-01-03 12:21:15 +0900

    Supports ads that cannot be skipped

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Press Enter key To Block [Youtube](https://youtube.com) Ad  
 
 ## Installation
-1. `git clone https://github.com/yorimoi/petbya.git` or Download ZIP  
+1. `git clone https://github.com/kumavale/petbya.git` or Download ZIP  
 2. Click the Chrome menu icon and select Extensions from the Tools menu. Ensure that the "Developer mode" checkbox in the top right-hand corner is checked.  
 3. Click on the "Load unpacked" button.  
 
--- content.js
+++ content.js
@@ -3,14 +3,24 @@ window.addEventListener('keydown', function(e) {
     const key_Enter = 13;
 
     if(e.keyCode == key_Enter) {
-        let ad1 = document.getElementsByClassName("ytp-ad-overlay-close-container")[0];
-        let ad2 = document.getElementsByClassName("ytp-ad-skip-button-container")[0];
+        let ad_elem = [
+            // Telop-style Ads
+            document.getElementsByClassName("ytp-ad-overlay-close-container")[0],
+            // Ads that can be skipped
+            document.getElementsByClassName("ytp-ad-skip-button-container")[0],
+        ];
+        ad_elem.forEach(element => {
+            if (element) {
+                element.click();
+            }
+        });
 
-        if (ad1) {
-            ad1.click();
-        }
-        if (ad2) {
-            ad2.click();
+        // Ads that cannot be skipped
+        let v_elem = document.getElementsByClassName("html5-main-video")[0];
+        if (v_elem) {
+            if (document.getElementsByClassName("ytp-ad-player-overlay-instream-info")[0]) {
+                v_elem.currentTime = 99999;
+            }
         }
     }
 });
